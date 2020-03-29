cf9680a382b62667c20e179528ce042912e04555
Sergio Roque <o92dupes@gmail.com>
2020-03-29 14:42:48 +0200

    Update Casos_COVID_Espana.py

--- Casos_COVID_Espana.py
+++ Casos_COVID_Espana.py
@@ -6,7 +6,7 @@ import pandas as pd
 # Definimos la función casos_covid con la que obtendremos la tabla resumen de los casos de coronavirus por país
 def casos_covid_espana():
     # Obtenemos los datos desde la url
-    r = requests.get("https://www.lavanguardia.com/vida/20200316/474186615740/evolucion-curva-coronavirus-por-comunidad-autonoma.html")
+    r = requests.get("https://www.rtve.es/noticias/20200329/mapa-del-coronavirus-espana/2004681.shtml")
     # Definimos un mensaje en caso de que no se le puedan obtener los datos
     msg = "No se pueden obtener datos"
     # Pasamos los datos a texto
@@ -14,7 +14,7 @@ def casos_covid_espana():
     # Definimos el parser
     soup = BeautifulSoup(data,'lxml')
     # Buscamos la tabla de donde extraeremos los datos
-    table = soup.find("table")
+    table = soup.find("div", id = 'table')
    
     # Definimos las condiciones que nos devolveran el mensaje de error
     if r.status_code != 200:
@@ -30,17 +30,18 @@ def casos_covid_espana():
     data = []
     
     # Creamos el bucle que iterará todas las filas y las columnas (o las que queramos) de la tabla y las añadimos a la lista
-    table_body = table.find('tbody')
+    table_body = table.find('div')
     rows = table_body.find_all('tr')
     for row in rows:
         cols = row.find_all('td')
         cols = [ele.text.strip() for ele in cols]
         data.append(cols)
-        
+    return "".join(data)
+    print( "".join(data).strip() )   
 
     # Imprimimos los resultados con los espacios y saltos de línea
     
-    df = pd.DataFrame(data, columns=['CCAA','Casos','Fallecidos','Casos/1000hab'])
+    df = pd.DataFrame(data)
     df.to_csv('Casos_COVID_Espana.csv')    
     
     

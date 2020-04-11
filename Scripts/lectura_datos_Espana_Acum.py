e190016ff14b9c244bfb0b2945a9c533d8e02738
Sergio Roque <o92dupes@gmail.com>
2020-04-12 00:17:22 +0200

    Más cambios

--- Scripts/lectura_datos_Espana_Acum.py
+++ Scripts/lectura_datos_Espana_Acum.py
@@ -5,13 +5,22 @@ import matplotlib.pyplot as plt
 data = pd.read_csv('csv\Casos_COVID_Espana_acumulado.csv') 
 # Creamos un gráfico en el que se ve la evolución de los casos totales en España desde 
 # el primer caso hasta la fecha
+fig, ax1=plt.subplots(figsize=(10,6))
 data.plot('FECHA','CASOS')
+ax1.set_xlabel("Fecha")
+ax1.set_ylabel("Nº de Casos")
+fig.suptitle('Casos Covid España', fontsize=15)
 plt.savefig('Graficos\CasosCOVIDEspaña')
 plt.show()
 
 # Guardamos también una gráfica en la que se observa la evolución de los casos por CCAA
+fig, ax2=plt.subplots(figsize=(10,6))
 data_g = data.groupby('CCAA')['CASOS']
 data_g.plot()
+ax2.set_xlabel("Fecha")
+ax2.set_ylabel("Nº de Casos")
+ax2.legend(loc="best", title="CCAA", frameon=True)
+fig.suptitle('Casos Covid CCAA', fontsize=15)
 plt.savefig('Graficos\CasosCOVIDCCAA')
 plt.show()
 
